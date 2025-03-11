3f1f1a21b7aa31463983f33a0d89d0d778c21aef
AminFiroozi <afiroozi007@gmail.com>
2025-03-12 02:51:15 +0330

    feat: add dashboard layout

---
+++ app/panel/layout.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import PanelAside from '@/components/PanelAside/PanelAside';
+// import '../styles/globals.css';
+import '@/styles/global.css';
+import { Gauge, Layers, FilePlus, History, BarChart, MessageSquare, Calculator } from "lucide-react";
+
+// const myFont = localFont({ src: '../..' })
+  
+
+
+
+export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
+
+    const navItems = [
+        { name: "داشبورد", path: "/panel/dashboard", icon: <Gauge /> },
+        { name: "پنل‌های من", path: "/panel/my-panels", icon: <Layers /> },
+        { name: "ثبت سفارش", path: "/panel/new-order", icon: <FilePlus /> },
+        { name: "سوابق تعمیرات", path: "/panel/repair-history", icon: <History /> },
+        { name: "گزارشات", path: "/panel/reports", icon: <BarChart /> },
+        { name: "پیام‌های من", path: "/panel/messages", icon: <MessageSquare /> },
+        { name: "محاسبه‌گر", path: "/panel/calculator", icon: <Calculator /> },
+    ];
+
+
+  return (
+    <PanelAside navItems={navItems} >
+      {children}
+    </PanelAside>
+  );
+};
