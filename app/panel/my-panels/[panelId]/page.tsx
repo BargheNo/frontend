e93264d1494ba303a5da795e81846b16daa494d0
AminFiroozi <afiroozi007@gmail.com>
2025-03-15 14:05:41 +0330

    feat(panel): use persian words and style the chart

--- app/panel/my-panels/[panelId]/page.tsx
+++ app/panel/my-panels/[panelId]/page.tsx
@@ -1,20 +1,23 @@
 import LiveChart from "@/components/Panel/LiveChart/LiveChart";
 
 interface PanelProps {
-    params: {
-        panelId: string;
-    };
+	params: {
+		panelId: string;
+	};
 }
 
 const PanelId = ({ params }: PanelProps) => {
-    const { panelId } = params; // Access the panelId from params
+	const { panelId } = params; // Access the panelId from params
 
-    return (
-        <div>
-            <h1>Panel ID: {panelId}</h1>
-			<LiveChart />
-        </div>
-    );
+	return (
+		<div>
+			<h1>Panel ID: {panelId}</h1>
+			<div className="grid grid-cols-2 gap-5 w-full px-5">
+				<LiveChart />
+				<LiveChart />
+			</div>
+		</div>
+	);
 };
 
 export default PanelId;
--- components/Panel/LiveChart/LiveChart.tsx
+++ components/Panel/LiveChart/LiveChart.tsx
@@ -3,13 +3,15 @@
 import React, { JSX } from "react";
 import ReactApexChart from "react-apexcharts";
 import { ApexOptions } from "apexcharts";
+import { vazir } from "@/lib/fonts";
 
 const XAXISRANGE: number = 60000;
 const lastDate: Date = new Date();
 
 const getNewSeries = (lastDate: Date, range: { min: number; max: number }) => {
 	const newDate = new Date(lastDate.getTime() + 1000); // Increment by 1 second
-	const newDataPoint = Math.floor(Math.random() * (range.max - range.min + 1)) + range.min; // Generate random data
+	const newDataPoint =
+		Math.floor(Math.random() * (range.max - range.min + 1)) + range.min; // Generate random data
 	return { x: newDate.getTime(), y: newDataPoint }; // Return new data point
 };
 
@@ -44,32 +46,40 @@ export default function LiveChart(): JSX.Element {
 			stroke: {
 				curve: "smooth",
 				width: 3,
-				colors: ['#FF4560'],
+				colors: ["#FF4560"],
 			},
 			title: {
-				text: "Dynamic Updating Chart",
-				align: "left",
+				text: "میزان برق تولیدی",
+				offsetX: -5,
+				offsetY: 10,
+				align: "right",
 				style: {
-					fontSize: '16px',
-					color: '#333',
+					fontSize: "20px",
+					color: "#333",
+					fontFamily: "vazir",
 				},
 			},
 			xaxis: {
 				type: "datetime",
 				range: XAXISRANGE,
 				title: {
-					text: 'Time',
+					text: "زمان",
+					offsetY: 5,
 					style: {
-						color: '#333',
+						color: "#333",
+						fontFamily: "vazir",
+						fontSize: "14px",
 					},
 				},
 			},
 			yaxis: {
 				max: 100,
 				title: {
-					text: 'Value',
+					text: "مگاوات",
 					style: {
-						color: '#333',
+						color: "#333",
+						fontFamily: "vazir",
+						fontSize: "14px",
 					},
 				},
 			},
@@ -79,15 +89,16 @@ export default function LiveChart(): JSX.Element {
 			tooltip: {
 				enabled: true,
 				style: {
-					fontSize: '12px',
-					color: '#333',
+					fontSize: "12px",
+					// colors: '#333',
 				},
 				x: {
-					format: 'dd MMM HH:mm',
+					format: "dd MMM HH:mm",
 				},
 			},
 			grid: {
-				borderColor: '#e0e0e0',
+				borderColor: "#e0e0e0",
+				// borderColor: "#000"
 			},
 		},
 	});
@@ -122,6 +133,7 @@ export default function LiveChart(): JSX.Element {
 					series={state.series}
 					type="line"
 					height={350}
+					className="w-full border-solid border-2 border-black"
 				/>
 			</div>
 			<div id="html-dist"></div>
