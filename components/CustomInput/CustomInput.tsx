75c4d28dde5db94c27e109ed126f1df55790903c
tinamohammdpour <115643729+tinamohammadpour@users.noreply.github.com>
2025-03-15 20:26:12 +0330

    Feature/signup (#35)
    * feat:implemented the basics of the signup page
    
    * feat:added the background image and organized styles
    
    * feat:added styles and icons
    
    * feat:added icons and responsive page
    
    * feat:styled checkbox and the linked rule
    
    * feat:completed the responsiveness of the card
    
    * feat:added the code for phonenumber field
    
    * fix: build errors
    
    ---------
    
    Co-authored-by: AminFiroozi <afiroozi007@gmail.com>
    Co-authored-by: Amin Firoozi <114510343+AminFiroozi@users.noreply.github.com>

---
+++ components/CustomInput/CustomInput.tsx
@@ -0,0 +1,34 @@
+import style from "./CustomInput.module.css";
+import { LucideIcon } from 'lucide-react';
+
+
+interface Props {
+  value?: string;
+  onChange?: (value: string) => void;
+  children: React.ReactNode; 
+  type:string;
+  style?: React.CSSProperties;
+  icon?:LucideIcon;
+  onIconClick?:()=>void;
+  readonly?:boolean;
+}
+
+export default function CustomInput({ value, onChange, children,type,icon:Icon,onIconClick,readonly }: Props) {
+  return (
+    <div className={style.Conter}>
+        {Icon&&<Icon onClick={onIconClick} className={style.icon}></Icon>}
+      {value === "" && <label
+        className={style.text}
+      >
+        {children}
+      </label>}
+      <input
+        type={type}
+        value={value}
+        readOnly={readonly}
+        onChange={(e) => onChange && onChange(e.target.value)}
+        className={`${style.CustomInput} ${style.numberInput}`}
+      />
+    </div>
+  );
+}
