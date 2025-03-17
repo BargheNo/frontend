646aba365a077a019078dfc7382e6ce3fd4fb499
Amin Firoozi <114510343+AminFiroozi@users.noreply.github.com>
2025-03-18 00:13:30 +0330

    Feature/panels (#42)
    * feat(panels): setup routing
    
    * feat(panels): add live chart to manage panel
    
    * feat(panel): improve chart design
    
    * feat(panel): use persian words and style the chart
    
    * fix(panel): server component errors about usage of window
    
    * feat(panel): add style to texts and spacings
    
    * feat(panel): Add panel info section to panel dashboard
    
    * fix(panel): bugs for build
    
    * fix: dependencies

---
+++ components/Panel/PanelCard/PanelCard.tsx
@@ -0,0 +1,154 @@
+import React from "react";
+import { MoveLeft, Sun, Battery, Gauge, MapPin } from "lucide-react";
+import { PanelCardProps } from "@/src/types/PanelCardTypes";
+import Link from "next/link";
+const i = Gauge;
+const PanelCard = ({
+	panelName,
+	technicalDetails,
+	address,
+	className,
+}: PanelCardProps) => {
+	// Determine status color based on efficiency
+	const getStatusColor = () => {
+		const efficiency = technicalDetails.efficiency;
+		if (efficiency >= 80)
+			return "bg-gradient-to-br from-green-400 to-green-500 border-1 border-gray-100/50 shadow-sm shadow-green-500";
+		if (efficiency >= 60)
+			return "bg-gradient-to-br from-yellow-400 to-yellow-500 shadow-yellow-500";
+		return "bg-gradient-to-br from-red-400 to-red-500 shadow-red-500";
+	};
+
+	// Format numbers with commas for better readability
+	const formatNumber = (num: number): string => {
+		return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	};
+
+	// Function to wrap icons with a rounded background
+	const IconWithBackground = ({
+		icon: Icon,
+		color,
+	}: {
+		icon: React.ElementType;
+		color: string;
+	}) => (
+		<div
+			className={`p-2 rounded-xl bg-[#eff0f1] shadow-[-4px_-4px_10px_rgba(255,255,255,1),2px_2px_5px_rgba(0,0,0,0.3)]`}
+		>
+			<Icon
+				className="w-4 h-4 transition-transform duration-200 hover:scale-125"
+				style={{ color }}
+			/>
+		</div>
+	);
+
+	return (
+		<div
+			className={`${className} w-full ${
+				0 ? "h-64" : ""
+			} border-t-1 border-gray-300 first:border-t-0`}
+		>
+			<div className="flex flex-row justify-between w-full h-full bg-[#F0EDEF] p-5 overflow-hidden relative">
+				{/* Left Section: Panel Details */}
+				<div className="flex flex-col justify-between w-full z-10">
+					<div className="space-y-3 w-full">
+						<h2 className="text-2xl font-bold text-gray-800">
+							{panelName}
+						</h2>
+						<div className="space-y-2 w-full">
+							<div className="flex text-sm text-gray-700 justify-between w-[28%] items-center">
+								<div className="flex items-center w-1/2">
+									<IconWithBackground
+										icon={Battery}
+										color="#6B7280"
+									/>
+									<span className="font-medium mr-2">
+										ظرفیت:
+									</span>
+								</div>
+								<div className="w-1/2">
+									<span className="mr-1">
+										{formatNumber(
+											technicalDetails.capacity
+										)}{" "}
+										کیلووات
+									</span>
+								</div>
+							</div>
+							<div className="flex text-sm text-gray-700 justify-between w-[28%] items-center">
+								<div className="flex items-center w-1/2">
+									<IconWithBackground
+										icon={Sun}
+										color="#F59E0B"
+									/>
+									<span className="font-medium mr-2">
+										تولید امروز:
+									</span>
+								</div>
+								<div className="w-1/2">
+									<span className="mr-1">
+										{formatNumber(
+											technicalDetails.todayProduction
+										)}{" "}
+										کیلووات ساعت
+									</span>
+								</div>
+							</div>
+							<div className="flex text-sm text-gray-700 justify-between w-[28%] items-center">
+								<div className="flex items-center w-1/2">
+									<IconWithBackground
+										icon={i}
+										color="#3B82F6"
+									/>
+									<span className="font-medium mr-2">
+										بازدهی:
+									</span>
+								</div>
+								<div className="w-1/2">
+									<span className="mr-1">
+										{technicalDetails.efficiency}%
+									</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="flex items-start text-sm text-gray-700 mt-6">
+						<div className="flex flex-row items-center">
+							<IconWithBackground icon={MapPin} color="#6B7280" />
+							<div className="font-medium mx-2">آدرس:</div>
+						</div>
+						<div className="flex flex-row mt-[6px]">
+							<span className="mr-1">{address}</span>
+						</div>
+					</div>
+				</div>
+
+				{/* Right Section: Status and Action Button */}
+				<div className="flex flex-col justify-around gap-4 items-center z-10 min-w-48">
+					<div className="flex flex-col gap-4">
+						<div className="text-gray-700 font-medium">
+							وضعیت پنل:
+						</div>
+						<div className="flex flex-col items-center justify-center gap-2 p-3 rounded-2xl bg-[#F0F0F3] shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.8),inset_4px_4px_10px_rgba(0,0,0,0.1)] w-24">
+							<div
+								className={`h-4 w-4 rounded-full ${getStatusColor()} shadow-md`}
+							></div>
+							<span className="text-sm font-medium text-gray-600">
+								فعال
+							</span>
+						</div>
+					</div>
+					<Link href={`/panel/my-panels/123`}>
+						{/* Replace 123 with panelId later */}
+						<button className="flex items-center justify-between bg-gradient-to-r from-[#EE4334] to-[#D73628] px-4 py-2 text-white cursor-pointer shadow-md rounded-full hover:shadow-lg transition duration-300 hover:scale-105">
+							<span className="font-medium">مدیریت پنل</span>
+							<MoveLeft className="mr-2 w-4 h-4" />
+						</button>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default PanelCard;
--- src/types/PanelCardTypes.ts
+++ src/types/PanelCardTypes.ts
@@ -8,4 +8,5 @@ export interface PanelCardProps {
   panelName: string;
   technicalDetails: TechnicalDetails;
   address: string;
+  className?: string | null;
 } 
