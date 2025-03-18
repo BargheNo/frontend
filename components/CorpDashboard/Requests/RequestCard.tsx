abec820a15db517debb8a3ed4dea856ea79c3a18
AminFiroozi <afiroozi007@gmail.com>
2025-03-18 20:50:58 +0330

    feat(requests): remove bid count and improve displaying numbers

--- components/CorpDashboard/Requests/RequestCard.tsx
+++ components/CorpDashboard/Requests/RequestCard.tsx
@@ -1,6 +1,5 @@
 import IconWithBackground from "@/components/IconWithBackground/IconWithBackground";
 import { RequestCardProps } from "@/src/types/RequestCardTypes";
-import { endSession } from "@sentry/nextjs";
 import {
 	ArrowLeft,
 	Battery,
@@ -11,21 +10,51 @@ import {
 } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+function naiveRound(num, decimalPlaces = 0) {
+	const p = Math.pow(10, decimalPlaces);
+	return Math.round(num * p) / p;
+}
 
-function checkValue(value: number | string, english: boolean) {
-	let optValue: string;
+function wordExpression(value: number | string, english: boolean) {
 	if (typeof value === "number") {
-		optValue = `${value}`;
-		if (value > 1e3)
-			optValue = english ? `${value / 1e3}k` : `${value / 1e3} هزار`;
-		if (value > 1e6)
-			optValue = english ? `${value / 1e6}M` : `${value / 1e6} میلیون`;
-		if (value > 1e9)
-			optValue = english ? `${value / 1e9}G` : `${value / 1e9} میلیارد`;
-		return { value: optValue, changed: true };
-	} else {
-		return { value: value, changed: false };
+		if (english) {
+			if (value > 1e3) return { value: `${value / 1e3}k`, changed: true };
+			if (value > 1e6) return { value: `${value / 1e6}M`, changed: true };
+			if (value > 1e9) return { value: `${value / 1e9}G`, changed: true };
+			return { value: `${value}`, changed: true };
+		} else {
+			let res = "";
+			let found = false;
+			if (Math.round(value / 1e12) !== 0) {
+				res += `${Math.round(value / 1e12)} تیلیارد`;
+				found = true;
+			}
+			if (Math.round(value / 1e9) % 1000 !== 0) {
+				if (found) res += " و ";
+				res += `${Math.round(value / 1e9) % 1000} میلیارد`;
+                found = true;
+			}
+            console.log("m:", Math.round(value / 1e9) % 1000)
+			if (Math.round(value / 1e6) % 1000 !== 0) {
+				if (found) res += " و ";
+				res += `${Math.round(value / 1e6) % 1000} میلیون`;
+                found = true;
+			}
+			if (Math.round(value / 1e3) % 1000 !== 0) {
+				if (found) res += " و ";
+				res += `${Math.round(value / 1e3) % 1000} هزار`;
+                found = true;
+			}
+            if (Math.round(value) % 1000 !== 0) {
+				if (found) res += " و ";
+				res += `${Math.round(value) % 1000}`;
+			}
+
+
+			return {value: res, changed: true};
+		}
 	}
+	return { value: value, changed: false };
 }
 
 const Item = ({
@@ -43,7 +72,7 @@ const Item = ({
 	prefix?: string;
 	english?: boolean;
 }) => {
-	const { value, changed } = checkValue(fieldValue, english);
+	const { value, changed } = wordExpression(fieldValue, english);
 	return (
 		<div className="flex my-2 h-full gap-3">
 			<div className="flex items-start">
@@ -113,12 +142,12 @@ export default function RequestCard({
 						/>
 					</div>
 					<div className="flex flex-col justify-evenly w-1/5 items-center text-center">
-						<div className="flex flex-col shadow-[-6px_-6px_16px_rgba(255,255,255,0.8),6px_6px_16px_rgba(0,0,0,0.2)] rounded-md px-4 mx-4 my-4 py-2">
+						{/* <div className="flex flex-col shadow-[-6px_-6px_16px_rgba(255,255,255,0.8),6px_6px_16px_rgba(0,0,0,0.2)] rounded-md px-4 mx-4 my-4 py-2">
 							<span>پیشنهادهای ارسال شده</span>
 							<span className="text-3xl font-bold" dir="rtl">
 								{sentRequestsCount}/5
 							</span>
-						</div>
+						</div> */}
 						<div className="flex flex-col items-center gap-2">
 							<Link href={"requests/meow"}>
 								<div className="bg-gradient-to-b from-[#EE4334] to-[#D73628] rounded-full w-16 h-16 flex items-center place-content-center text-white cursor-pointer shadow-md hover:shadow-lg transition duration-300 hover:scale-105">
