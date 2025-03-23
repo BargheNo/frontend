04a4f3d042e43a8b2301e7d6644c4cecd5717dfe
Tina.2017 <tina.mpmp2017@gmail.com>
2025-03-23 15:45:01 +0330

    feat:order request connect to back is all done

---
+++ src/types/OrderrequestType.ts
@@ -0,0 +1,16 @@
+interface order{
+    
+    name: string,
+    area:number,
+    power: number,
+    maxCost: number,
+    buildingType: string,
+    description: string,
+    provinceID: number,
+    cityID: number,
+    streetAddress: string,
+    postalCode:string,
+    houseNumber: string,
+    unit: number
+}
+export type{order}
