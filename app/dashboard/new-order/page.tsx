a0de1f4380457189e0da394d45683acf30434615
tinamohammdpour <115643729+tinamohammadpour@users.noreply.github.com>
2025-03-24 17:40:46 +0330

    Feature/order history (#75)
    * feat:added the order history card component
    
    * feat:added the service
    
    * feat:setup pagination
    
    * feat:connected orderhistory to back and seperated its component
    
    * feat:improve the style of address
    
    * fix:changed the request header and token

--- app/dashboard/new-order/page.tsx
+++ app/dashboard/new-order/page.tsx
@@ -1,22 +1,27 @@
 import { vazir } from '@/lib/fonts';
-
 import Neworder from '@/components/New-Order/new-order';
+import OrderHistoryPagination from '@/components/OrderHistory/OrderHistoryPagination';
+
+  
 
 export default function Page() {
     
-
     return (
         <>
         <div className={`${"flex justify-center items-center mt-15"} ${vazir.className}`}>
             <div>
                <Neworder></Neworder>
 
-                <div className='mr-2 mt-4 text-navy-blue'>
+                <div className='mr-2 mt-3 text-navy-blue'>
                     <p>ثبت سفارش جدید</p>
                 </div>
             </div>
         </div>
-    
+        <div className=' flex flex-row font-bold text-navy-blue text-2xl mr-8 mt-6 mb-3 justify-start '>
+                  <p> سابقه سفارشات </p>
+        </div>
+        <OrderHistoryPagination></OrderHistoryPagination>
+        
         </>
     )
 }
