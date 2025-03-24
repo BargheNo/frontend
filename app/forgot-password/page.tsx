2d5dd06b238c8f5c2541ef69e121d638e464645c
Amirsaeed <amirsaeed7bn@gmail.com>
2025-03-24 18:01:20 +0330

    feature : handled the forgot password routing

---
+++ app/forgot-password/page.tsx
@@ -0,0 +1,11 @@
+import ForgotPassword from "@/components/Auth/ForgotPassword/ForgotPassword";
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <ForgotPassword />
+  )
+}
+
+export default page
