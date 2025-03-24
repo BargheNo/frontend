8681c7f28d57219afd1c52883fe32a9a04afaa96
Amirsaeed <amirsaeed7bn@gmail.com>
2025-03-24 12:11:53 +0330

    feature : handled reset password's routing

---
+++ app/reset-password/page.tsx
@@ -0,0 +1,11 @@
+import ResetPassword from "@/components/Auth/ResetPassword/ResetPassword";
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <ResetPassword />
+  )
+}
+
+export default page
